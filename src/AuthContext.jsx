8d3a42fe59bd65b9700434f78ad1cb0759e493e4
Emerson Recto <emersontrecto@gmail.com>
2025-05-19 17:06:40 -0400

    finished authentication/signup

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -9,10 +9,48 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  async function signup(name) {
+    try {
+      const response = await fetch(API + "/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: name,
+        }),
+      });
+      const result = await response.json();
+      setToken(result.token);
+      setLocation("TABLET");
+      console.log(result, "\n");
+
+      // This will depend on the API you're actually working with
+      return result.token;
+    } catch (e) {
+      console.error("oh no ;(");
+    }
+  }
 
   // TODO: authenticate
+  async function authenticate(token) {
+    try {
+      const response = await fetch(API + "/authenticate", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const result = await response.json();
+      setLocation("TUNNEL");
+      return result;
+    } catch (e) {
+      console.error("Authentication issue:(");
+    }
+  }
 
-  const value = { location };
+  const value = { location, signup, authenticate };
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
